a64d2174193b9eb79ffcd9bb0c76d7aee598afb5
1217967340@qq.com <1217967340@qq.com>
2022-01-23 14:39:13 +0800

    feat: 添加jsonserve

---
+++ server.js
@@ -0,0 +1,82 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+const jsonServer = require('json-server')
+const bodyParser = require('body-parser')
+const server = jsonServer.create()
+const router = jsonServer.router('./mock/db.json')
+const middlewares = jsonServer.defaults()
+const jwt = require('jsonwebtoken')
+
+const KEY = '1234'
+// 创建token
+const createToken = (playload) => {
+  return jwt.sign(playload, KEY, {
+    expiresIn: '1h',
+  })
+}
+// 验证token
+const verifyToken = (token) => {
+  jwt.verify(token, KEY)
+}
+
+// 添加前缀
+const rewriter = jsonServer.rewriter({
+  '/api/*': '/$1',
+})
+
+// 解析body为json
+server.use(bodyParser.json())
+server.use(rewriter)
+server.use(middlewares)
+
+// 添加works 验证token 中间件
+server.use('/works', (req, res, next) => {
+  const errorResp = {
+    erroron: 1200,
+    message: '登录校验失败',
+  }
+  const authHeader = req.headers.authorization
+  if (!authHeader) {
+    res.json(errorResp)
+    return
+  }
+  try {
+    verifyToken(authHeader.split(' ')[1])
+    next()
+  } catch {
+    res.json(errorResp)
+  }
+})
+
+// 自定义接口
+server.get('/echo', (req, res) => {
+  res.json({
+    data: '123',
+  })
+})
+
+// 自定义接口生成token
+server.post('/users/loginByPhoneNumber', (req, res) => {
+  const { phoneNumber, veriCode } = req.body
+  const token = createToken({ phoneNumber, veriCode })
+  res.status(200).json({
+    data: {
+      token,
+    },
+  })
+})
+
+// 修改数据结构
+router.render = (req, res) => {
+  res.json({
+    errorno: 0,
+    data: {
+      list: res.locals.data,
+      count: res.locals.data.length,
+    },
+  })
+}
+
+server.use(router)
+server.listen(3000, () => {
+  console.log('JSON Server is running')
+})
